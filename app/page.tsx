e0b2c5477287ea7afd1485ab33f83ba166f92acd
Daniel Lehre Ekeberg <danieleke@live.no>
2026-01-06 23:48:10 +0100

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 'use client';
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    // window.location.href = "https://www.csstatlab.com"
-  })
-  return (
-    <div className="h-screen bg-black" />
-  );
+    window.location.href = 'https://www.csstatlab.com';
+  });
+  return <div className="h-screen bg-zinc-950" />;
 }
